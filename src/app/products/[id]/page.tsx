d7f3194e5900d4a71d7b731821f75dfb58392f2a
Rudra Vashishtha <rudraturtle7@gmail.com>
2023-10-20 04:18:32 +0530

    Update products page

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,64 +1,63 @@
-import notFound from "@/app/not-found";
-import { PriceTag } from "@/components/PriceTag";
+import PriceTag from "@/components/PriceTag";
 import { prisma } from "@/lib/db/prisma";
+import { Metadata } from "next";
 import Image from "next/image";
+import { notFound } from "next/navigation";
 import { cache } from "react";
 import AddToCartButton from "./AddToCartButton";
 import { incrementProductQuantity } from "./actions";
 
 interface ProductPageProps {
-    params: {
-        id: string,
-    }
+  params: {
+    id: string;
+  };
 }
 
 const getProduct = cache(async (id: string) => {
-    const product = await prisma.product.findUnique({where: {id}})
-    if (!product) notFound();
-    return product;
+  const product = await prisma.product.findUnique({ where: { id } });
+  if (!product) notFound();
+  return product;
 });
 
-interface MetaData {
-    title: string;
-    description: string;
-    image: string;
+export async function generateMetadata({
+  params: { id },
+}: ProductPageProps): Promise<Metadata> {
+  const product = await getProduct(id);
+
+  return {
+    title: product.name + " - Flowmazon",
+    description: product.description,
+    openGraph: {
+      images: [{ url: product.imageUrl }],
+    },
+  };
 }
 
-export async function generateMetaData(
-    { params: {id} }: ProductPageProps
-) : Promise<MetaData> {
-    const product = await getProduct(id);
+export default async function ProductPage({
+  params: { id },
+}: ProductPageProps) {
+  const product = await getProduct(id);
 
-    return {
-        title: product.name + ' - Flowmmazon',
-        description: product.description,
-        openGraph: {
-            images: [{ url: product.imageUrl }]
-        }
-    }
-}
+  return (
+    <div className="flex flex-col gap-4 lg:flex-row lg:items-center">
+      <Image
+        src={product.imageUrl}
+        alt={product.name}
+        width={500}
+        height={500}
+        className="rounded-lg"
+        priority
+      />
 
-export default async function ProductPage(
-    { params: {id} }: ProductPageProps
-) {
-    const product = await getProduct(id);
-
-    return (
-        <div className="flex flex-col lg:flex-row lg:items-center gap-5">
-            <Image
-                src={product.imageUrl}
-                alt={product.name}
-                width={500}
-                height={500}
-                className="rounded-lg"
-                priority
-            />
-            <div>
-                <h1 className="text-5xl font-bold mt-5">{product.name}</h1>
-                <PriceTag price={product.price} className="mt-4" />
-                <p className="py-6">{product.description}</p>
-                <AddToCartButton productId={product.id} incrementProductQuantity={incrementProductQuantity} />
-            </div>
-        </div>
-    );
+      <div>
+        <h1 className="text-5xl font-bold">{product.name}</h1>
+        <PriceTag price={product.price} className="mt-4" />
+        <p className="py-6">{product.description}</p>
+        <AddToCartButton
+          productId={product.id}
+          incrementProductQuantity={incrementProductQuantity}
+        />
+      </div>
+    </div>
+  );
 }
--- src/components/PriceTag.tsx
+++ src/components/PriceTag.tsx
@@ -1,14 +1,10 @@
 import { formatPrice } from "@/lib/format";
 
 interface PriceTagProps {
-    price: number;
-    className?: string;
+  price: number;
+  className?: string;
 }
 
-export function PriceTag({ price, className }: PriceTagProps) {
-  return (
-    <span className={`badge ${className}`}>
-        {formatPrice(price)}
-    </span>
-  )
+export default function PriceTag({ price, className }: PriceTagProps) {
+  return <span className={`badge ${className}`}>{formatPrice(price)}</span>;
 }
